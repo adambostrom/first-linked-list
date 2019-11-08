354c63c2e5a402959774e211aa10ff4d7f10e000
Adam Boström <abm19002@student.mdh.se>
2019-11-08 20:01:22 +0100

    Add project files.

---
+++ first-linked-list/main.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int val;
+	struct node * next;
+} node_t;
+
+int main() {
+	
+	node_t *head = NULL;
+	head = (node_t*)malloc(sizeof(node_t));
+	if (head == NULL) {
+		return 1;
+	}
+
+	head->val = 1;
+	head->next = (node_t*)malloc(sizeof(node_t));
+	
+
+	head->next->val = 2;
+	head->next->next = NULL;
+
+
+
+	system("pause");
+	return 0;
+}
